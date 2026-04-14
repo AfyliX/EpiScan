5804d787aa92f60d781cc2c8ccd6694b7436c159
gabriel spanneut <gabriel.spanneut-peressutti@epitech.eu>
2026-04-14 10:39:47 +0200

    [+] Added CLI in the main with --code, report and help with cmakelist for episcan exec

---
+++ build/CMakeFiles/episcan.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/episcan.dir/src/main.cpp.o"
+  "CMakeFiles/episcan.dir/src/main.cpp.o.d"
+  "episcan"
+  "episcan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/episcan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
